b36abe5690a6a0c4e7fdfb95eb222027f9277c9f
Ragub0413 <ragubangelica13@gmail.com>
2024-09-21 15:04:30 -0700

    initals

--- index.mjs
+++ index.mjs
@@ -6,6 +6,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Welcome to my server!');
 });
+app.get('/helloline', (req, res) => {
+    res.send('nextline!');
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
