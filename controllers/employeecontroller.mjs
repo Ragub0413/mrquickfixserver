affb8983c194b24f276bd3066818268b560856b4
Ragub0413 <ragubangelica13@gmail.com>
2024-09-21 22:02:32 -0700

    Check Inputs

--- controllers/employeecontroller.mjs
+++ controllers/employeecontroller.mjs
@@ -7,24 +7,24 @@ import employeemodel from '../model/employeemodel.mjs';
 const secret='test';
 
 const router = express.Router();
-export const storage = multer.diskStorage({
-    destination: function (req,file,cb){
-        cb(null,'public/profile');
+// export const storage = multer.diskStorage({
+//     destination: function (req,file,cb){
+//         cb(null,'public/profile');
 
-    },
-    filename: function (req,file,cb){
-        const uniqueSuffix = Date.now();
-        cb(null,uniqueSuffix + file.originalname);
-    }, 
-});
-const upload = multer({storage:storage});
+//     },
+//     filename: function (req,file,cb){
+//         const uniqueSuffix = Date.now();
+//         cb(null,uniqueSuffix + file.originalname);
+//     }, 
+// });
+// const upload = multer({storage:storage});
 
 export const createNewEmployee = async(req,res)=>{
     const {firstName,lastName,email,password,role,contactNumber} = req.body;
     console.log(firstName);
     try{
        // var dbo = db.dat
-         const imageName = req.file.filename;
+        // const imageName = req.file.filename;
     //    const oldEmployee = await Employee.findOne({_id});
     //    if(oldEmployee) return res.status(400).json({message:'Staff already exist'});
         const hashedPassword = await bcryptjs.hash(password,12);
--- router/employee.mjs
+++ router/employee.mjs
@@ -1,8 +1,8 @@
 import express from 'express'
-import { createNewEmployee,storage } from '../controllers/employeecontroller.mjs'
+import { createNewEmployee } from '../controllers/employeecontroller.mjs'
 import multer from 'multer';
 const router = express.Router();
-const upload = multer({storage:storage});
+// const upload = multer({storage:storage});
 router.post('/createEmployee',createNewEmployee);
 
 export default router;
