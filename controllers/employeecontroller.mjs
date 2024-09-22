eb944ef5dc2d87384e19c65a6638d44fc2e2f8d4
Ragub0413 <ragubangelica13@gmail.com>
2024-09-22 00:15:12 -0700

    Tested Field

--- controllers/employeecontroller.mjs
+++ controllers/employeecontroller.mjs
@@ -7,32 +7,33 @@ import employeemodel from '../model/employeemodel.mjs';
 const secret='test';
 
 const router = express.Router();
-// export const storage = multer.diskStorage({
-//     destination: function (req,file,cb){
-//         cb(null,'public/profile');
+export const storage = multer.diskStorage({
+    destination: function (req,file,cb){
+        cb(null,'public/profile');
 
-//     },
-//     filename: function (req,file,cb){
-//         const uniqueSuffix = Date.now();
-//         cb(null,uniqueSuffix + file.originalname);
-//     }, 
-// });
-// const upload = multer({storage:storage});
+    },
+    filename: function (req,file,cb){
+        const uniqueSuffix = Date.now();
+        cb(null,uniqueSuffix + file.originalname);
+    }, 
+});
+
+const upload = multer({storage:storage});
 
 export const createNewEmployee = async(req,res)=>{
-    const {firstName,lastName,email,password,role,contactNumber} = req.body;
-    console.log(firstName);
+    const {firstName,lastName,email,password,role,contactNumber,profilePicture} = req.body;
+    console.log(profilePicture);
     try{
        // var dbo = db.dat
-        // const imageName = req.file.filename;
+         const imageName = req.file.filename;
     //    const oldEmployee = await Employee.findOne({_id});
     //    if(oldEmployee) return res.status(400).json({message:'Staff already exist'});
         const hashedPassword = await bcryptjs.hash(password,12);
-        const result = await employeemodel.create({firstName,lastName,email,role,password: hashedPassword,contactNumber});
+        const result = await employeemodel.create({firstName,lastName,email,role,password: hashedPassword,contactNumber,profilePicture:imageName });
         res.status(201).json({result});
     }
     catch(err){ 
-        res.status(500).json({message:err}); 
-        console.log(err)
+        res.status(500).json({message: "Something went wrong"}); 
+        console.log(err);
     }
 } 
